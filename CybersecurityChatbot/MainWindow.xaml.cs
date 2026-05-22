340c71f15648e70b90b4ef5026b74095f79b6e7b
Selwyn Mukwevho <ST10465950@rcconnect.edu.za>
2026-05-22 20:42:58 +0200

    Add project files.

---
+++ CybersecurityChatbot/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Media;
+
+namespace CybersecurityChatbot
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // Sound
+            SoundPlayer player = new SoundPlayer();
+            player.SoundLocation = @"C:\Users\mukwe\source\repos\CybersecurityChatbot\CybersecurityChatbot\CyberGreeting.wav";
+            player.Play();
+
+            // ASCII Art
+            AsciiArt.Text =
+        @"███████╗ █████╗ ███████╗███████╗████████╗██╗   ██╗
+██╔════╝██╔══██╗██╔════╝██╔════╝╚══██╔══╝╚██╗ ██╔╝
+███████╗███████║█████╗  █████╗     ██║    ╚████╔╝
+╚════██║██╔══██║██╔══╝  ██╔══╝     ██║     ╚██╔╝
+███████║██║  ██║██║     ███████╗   ██║      ██║
+╚══════╝╚═╝  ╚═╝╚═╝     ╚══════╝   ╚═╝      ╚═╝
+
+███████╗██╗██████╗ ███████╗████████╗
+██╔════╝██║██╔══██╗██╔════╝╚══██╔══╝
+█████╗  ██║██████╔╝███████╗   ██║
+██╔══╝  ██║██╔══██╗╚════██║   ██║
+██║     ██║██║  ██║███████║   ██║
+╚═╝     ╚═╝╚═╝  ╚═╝╚══════╝   ╚═╝";
+        }
+
+
+        private void SendButton_Click(object sender, RoutedEventArgs e)
+        {
+            string userMessage = UserInput.Text;
+
+            ChatDisplay.Text += "\nYou: " + userMessage;
+
+            ChatDisplay.Text += "\nBot: Stay safe online!";
+
+            UserInput.Clear();
+        }
+    }
+}
